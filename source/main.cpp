7d67f2d134d84eb65ad60fb5ee23a89492bb17c3
Maurus Kühne <mauruskuehne@gmail.com>
2015-01-03 17:21:01 +0100

    added initial buildscripts and empty code files

---
+++ source/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MKPi
+//
+//  Created by Maurus Kühne on 03.01.15.
+//  Copyright (c) 2015 Maurus Kühne. All rights reserved.
+//
+
+#include "main.h"
+
+
+int main() {
+  return 0;
+}
---
+++ source/main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  MKPi
+//
+//  Created by Maurus Kühne on 03.01.15.
+//  Copyright (c) 2015 Maurus Kühne. All rights reserved.
+//
+
+#ifndef __MKPi__main__
+#define __MKPi__main__
+
+
+#endif /* defined(__MKPi__main__) */
