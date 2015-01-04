75967001067f149c7a1717546b0c696dbebfacfa
Maurus Kühne <mauruskuehne@gmail.com>
2015-01-04 12:37:15 +0100

    added led blinking! and fixed C++ compiling

--- source/main.cpp
+++ source/main.cpp
@@ -8,7 +8,33 @@
 
 #include "main.h"
 
+extern "C" void PUT32 ( unsigned int, unsigned int );
+extern "C" unsigned int GET32 ( unsigned int );
+extern "C" void dummy ( unsigned int );
+
+
+#define GPFSEL1 0x20200004
+#define GPSET0  0x2020001C
+#define GPCLR0  0x20200028
 
 int main() {
+  
+  //char* x = nullptr;
+  
+  unsigned int ra;
+  
+  ra=GET32(GPFSEL1);
+  ra&=~(7<<18);
+  ra|=1<<18;
+  PUT32(GPFSEL1,ra);
+  
+  while(1)
+  {
+    PUT32(GPSET0,1<<16);
+    for(ra=0;ra<0x100000;ra++) dummy(ra);
+    PUT32(GPCLR0,1<<16);
+    for(ra=0;ra<0x100000;ra++) dummy(ra);
+  }
+  
   return 0;
 }
