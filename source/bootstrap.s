1ebf61935a67a294ba4bb45ea00e3a30789f6902
Maurus Kühne <mauruskuehne@gmail.com>
2015-01-04 20:19:16 +0100

    started implementing interrupts

--- source/bootstrap.s
+++ source/bootstrap.s
@@ -1,21 +1,84 @@
 .global init
 .extern main
+
+
+;@ This is the actual entr point to the application.
+;@ I need to look into how to do this another way, but it seems like
+;@ this is the most reliable method for copying the interrupt handlers
+;@ into the IVT.
+interrupt:
+
+	;@ Basically each line represents the value to a branch command.
+	;@ Since this is the first thing, we know that our _relative_
+	;@ interrupt vector table will always be at the start of our
+	;@ kernel ( location: 0x8000 ).
+
+	;@ Reset Interrupt
+	ldr pc, reset_handler
+	
+	;@ Undefined Instruction
+	ldr pc, basic_handler
+	
+	;@ Software Interrupt
+	ldr pc, basic_handler
+	
+	;@ Prefetch Abort
+	ldr pc, basic_handler
+	
+	;@ Data Abort
+	ldr pc, basic_handler
+	
+	;@ ?? Looks like the ARM ARM specifys that
+	;@ there is just a casual hole here, so I guess we
+	;@ are filling it with another handler. Though I don't
+	;@ really know what calls this.
+	ldr pc, basic_handler
+	
+	;@ IRQ
+	ldr pc, basic_handler
+	
+	;@ FIQ
+	ldr pc, basic_handler
+
+
+
+;@ Next up is a trick to basically force the compiler
+;@ to store the branch command as a value
+;@ into the respective register.
+reset_handler:
+	.word reset
+
+basic_handler:
+	.word arm_interrupt_handler
+
+;@ Then this is our "reset" function
+;@ which is what will actually get automatically
+;@ called since the program is going to flow down
+;@ into this method.
+reset:
+
+	;@ So now we setup everything to copy the values over.
+	mov r0, #0x8000
+	mov r1, #0x0000
+
+	;@ And then we do the actual data copy.
+	ldmia r0!,{r2,r3,r4,r5,r6,r7,r8,r9}
+	stmia r1!,{r2,r3,r4,r5,r6,r7,r8,r9}
+	ldmia r0!,{r2,r3,r4,r5,r6,r7,r8,r9}
+	stmia r1!,{r2,r3,r4,r5,r6,r7,r8,r9}
+	
+	;@ And then call the bootstrapper init function.
+	;@ NOTE: I delegated these two assembly files because I don't
+	;@ want to marry my kernel to the ivt setup code.
+	b init
+	bx lr
+
+
 .globl _start
 _start:
 init:	
 ;@ Move the stack pointer.
 	mov sp, #0x80000
-
-/*
-;@light up ACT
-ldr r0,=0x20200000
-mov r1,#1
-lsl r1,#18
-str r1,[r0,#4]
-mov r1,#1
-lsl r1,#16
-str r1,[r0,#40]
-*/
 
 ;@ Invoke our main C++ entrypoint.
 	bl main
@@ -39,3 +102,21 @@ GET32:
 .global dummy
 dummy:
     bx lr
+
+hang:
+	b hang
+	
+.extern irq_handler
+;@ And here is the actual interrupt handler code.
+arm_interrupt_handler:
+
+	;@ Store the return link.
+	sub r14, r14, #4
+	stmfd sp!, {r0,r1,r2,r3,r4,r14}
+	
+	;@ Invoke our C++ irq handler.
+	bl irq_handler
+	
+	;@ Restore to the original caller.
+	ldmfd sp!, {r0,r1,r2,r3,r4,pc}^
+	bx lr
--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "main.h"
+#include <cstdint>
 
 extern "C" void PUT32 ( unsigned int, unsigned int );
 extern "C" unsigned int GET32 ( unsigned int );
@@ -17,14 +18,54 @@ extern "C" void dummy ( unsigned int );
 #define GPSET0  0x2020001C
 #define GPCLR0  0x20200028
 
+void blink() {
+  unsigned int ra;
+  PUT32(GPSET0,1<<16);
+  for(ra=0;ra<0x100000;ra++) dummy(ra);
+  PUT32(GPCLR0,1<<16);
+  for(ra=0;ra<0x100000;ra++) dummy(ra);
+}
+
+volatile bool interruptExecuted = false;
+
+bool irq_enable( void ) {
+  
+  volatile uint32_t* address = (volatile uint32_t*)( 0x2000b000 );
+  
+  // ENABLE IRQ 1
+  //*( address + 0x210 ) = (uint32_t)0x1000000000000; //0xFFFFFFFF;
+  
+  // ENABLE IRQ 2
+  //*( address + 0x214 ) = 0xFFFFFFFF;
+  
+  // ENABLE BASIC IRQ
+  //*( address + 0x218 ) = 0x000000FF;
+  
+  return true;
+}
+
 int main() {
-    
   unsigned int ra;
   
   ra=GET32(GPFSEL1);
   ra&=~(7<<18);
   ra|=1<<18;
   PUT32(GPFSEL1,ra);
+  
+  blink();
+  
+  interruptExecuted = false;
+  
+  blink();
+  
+  irq_enable();
+  
+  blink();
+  asm volatile( "SWI #0x0000FF" );
+  blink();
+  while (!interruptExecuted) {
+    dummy(0);
+  }
   
   while(1)
   {
@@ -35,4 +76,9 @@ int main() {
   }
   
   return 0;
+}
+
+extern "C" void irq_handler( void ) {
+  blink();
+  //interruptExecuted = true;
 }
