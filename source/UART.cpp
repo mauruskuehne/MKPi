b719faf5c2914f66a284f5b2833ad5b8cc5bf1cc
Maurus Kühne <mauruskuehne@gmail.com>
2015-01-19 21:19:02 +0100

    added a few comments

--- source/UART.cpp
+++ source/UART.cpp
@@ -20,6 +20,7 @@ extern "C" void dummy ( unsigned int );
 #define GPPUD       0x20200094
 #define GPPUDCLK0   0x20200098
 
+#define AUX_IRQ         0x20215000
 #define AUX_ENABLES     0x20215004
 #define AUX_MU_IO_REG   0x20215040
 #define AUX_MU_IER_REG  0x20215044
@@ -49,29 +50,35 @@ namespace HIL {
   UART::UART() {
     
     uint32_t ra;
-    Memory::PUT32(AUX_ENABLES,1);
-    Memory::PUT32(AUX_MU_IER_REG,0);
-    Memory::PUT32(AUX_MU_CNTL_REG,0);
-    Memory::PUT32(AUX_MU_LCR_REG,3);
-    Memory::PUT32(AUX_MU_MCR_REG,0);
-    Memory::PUT32(AUX_MU_IER_REG,0);
-    Memory::PUT32(AUX_MU_IIR_REG,0xC6);
-    Memory::PUT32(AUX_MU_BAUD_REG,270);
+    Memory::PUT32(AUX_ENABLES,1);       // Enable Auxiliary
+    Memory::PUT32(AUX_MU_IER_REG,0);    // Mini UART Interrupt Enable
+    Memory::PUT32(AUX_MU_CNTL_REG,0);   // Mini UART Extra Control
+    Memory::PUT32(AUX_MU_LCR_REG,3);    // Mini UART Line Control
+    Memory::PUT32(AUX_MU_MCR_REG,0);    // Mini UART Modem Control
+    Memory::PUT32(AUX_MU_IER_REG,0);    // Mini UART Interrupt Enable
+    Memory::PUT32(AUX_MU_IIR_REG,0xC6); // Mini UART Interrupt Identify
+    Memory::PUT32(AUX_MU_BAUD_REG,270); // Mini UART Baudrate
     
+    // GPIO Function Select 1
     ra=Memory::GET32(GPFSEL1);
     ra&=~(7<<12); //gpio14
     ra|=2<<12;    //alt5
+    // GPIO Function Select 1
     Memory::PUT32(GPFSEL1,ra);
     
-    
+    //GPIO Pin Pull-up/down Enable
     Memory::PUT32(GPPUD,0);
     for(ra=0;ra<150;ra++) dummy(ra);
+    
+    // GPIO Pin Pull-up/down Enable Clock 0
     Memory::PUT32(GPPUDCLK0,(1<<14));
     for(ra=0;ra<150;ra++) dummy(ra);
+    
+    // GPIO Pin Pull-up/down Enable Clock 0
     Memory::PUT32(GPPUDCLK0,0);
     
     
-    Memory::PUT32(AUX_MU_CNTL_REG,2);
+    Memory::PUT32(AUX_MU_CNTL_REG,2); // Mini UART Extra Control
   }
   
   void UART::sendText(char* text, int length) {
