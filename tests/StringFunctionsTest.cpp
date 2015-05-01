516198b28ea8e382b913e39549341f4ee696bf33
Maurus Kühne <mauruskuehne@gmail.com>
2015-05-01 17:07:10 +0200

    added catch unit tests for string functions

---
+++ tests/StringFunctionsTest.cpp
@@ -0,0 +1,24 @@
+//
+//  StringFunctionsTest.cpp
+//  MKPi
+//
+//  Created by Maurus Kühne on 01.05.15.
+//  Copyright (c) 2015 Maurus Kühne. All rights reserved.
+//
+
+#include <stdio.h>
+#include "catch.hpp"
+#include "StringFunctions.h"
+
+TEST_CASE( "strlen", "[StringFunctions]" ) {
+  
+  REQUIRE(Strings::strlen("012345") == 6);
+  REQUIRE(Strings::strlen("") == 0);
+}
+
+
+TEST_CASE( "strcmp", "[StringFunctions]" ) {
+  
+  REQUIRE(Strings::strcmp("first", "second") == 0);
+  REQUIRE(Strings::strlen("") == 0);
+}
---
+++ tests/main.cpp
@@ -0,0 +1,12 @@
+//
+//  main.cpp
+//  mkpi-tests
+//
+//  Created by Maurus Kühne on 01.05.15.
+//  Copyright (c) 2015 Maurus Kühne. All rights reserved.
+//
+
+#include <iostream>
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
